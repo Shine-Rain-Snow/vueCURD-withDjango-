d25d9ff20e71e9824150c7870e6481fd452a5610
Shine-Rain-Snow <sven916@hotmail.com>
2019-09-09 23:07:35 +0900

    vue django crud

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <h2><router-link to="/index">Subscriptions</router-link></h2>
+        <router-link to="/create" class="btn btn-sm btn-primary">
+          Add subscription
+        </router-link>
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
